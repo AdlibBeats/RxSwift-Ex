dfbb7cf6cd97c8964954b4e9259abb1368fb382c
adlibbeats <adlibbeats@icloud.com>
2021-02-17 16:24:01 +0300

    fix

--- RxSwift-Ex/MainViewController.swift
+++ RxSwift-Ex/MainViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import Swinject
 import SwinjectStoryboard
 
+//fix
+
 final class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
